64807d747dc79b037d8f66d2debb1050c4d6a139
danieljml <danielgarrido425@gmail.com>
2022-04-01 18:36:31 -0400

    Homepage styles and content

--- sections/admin/src/templates/header.php
+++ sections/admin/src/templates/header.php
@@ -12,19 +12,23 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../../src/styles/index-styles.css">
     <link rel="stylesheet" href="../../../../src/styles/global.css">
     <link rel="stylesheet" href="../../../../src/styles/form-styles.css" />
     <link rel="stylesheet" href="../styles/global.css">
+    <link rel="stylesheet" href="../styles/home-content.css">
 
 
     <title>Web site admin</title>
 </head>
+
 <body>
-    
+
     <?php 
         include("../../../../src/functions/util.php");
         $url=getWebPath();
@@ -32,16 +36,18 @@ session_start();
 
 
     <header class="container__header">
-      <nav class="nav__list">
-        <li class="nav__item"><a class="nav__link" href="#"><strong>Admin settings</strong></a></li>
-        <li class="nav__item"><a class="nav__link" href="<?php echo $url;?>/sections/admin/src/controllers/home.php">Home</a></li>
-        <li class="nav__item"><a class="nav__link" href="<?php echo $url;?>/sections/admin/src/controllers/courses.php">Courses</a></li>
-        <li class="nav__item"><a class="nav__link" href="<?php echo $url;?>/sections/admin/src/controllers/students.php">Students</a></li>
-        <li class="nav__item"><a class="nav__link" href="<?php echo $url;?>/sections/admin/src/controllers/signout.php">Sign out</a></li>
-      </nav>
+        <nav class="nav__list">
+            <li class="nav__item"><a class="nav__link" href="#"><strong>Admin settings</strong></a></li>
+            <li class="nav__item"><a class="nav__link"
+                    href="<?php echo $url;?>/sections/admin/src/controllers/home.php">Home</a></li>
+            <li class="nav__item"><a class="nav__link"
+                    href="<?php echo $url;?>/sections/admin/src/controllers/courses.php">Courses</a></li>
+            <li class="nav__item"><a class="nav__link"
+                    href="<?php echo $url;?>/sections/admin/src/controllers/students.php">Students</a></li>
+            <li class="nav__item"><a class="nav__link"
+                    href="<?php echo $url;?>/sections/admin/src/controllers/signout.php">Sign out</a></li>
+        </nav>
     </header>
 
     <div class="container">
         <div class="row">
-
-
---
+++ sections/admin/src/views/home-content.php
@@ -0,0 +1,11 @@
+<div class="container__home">
+    <h1 class="home_title">Welcome <?php echo $_SESSION['user']?></h1>
+    <div class="home_container_btn">
+        <a class="app__btn left" href="#">Courses</a>
+        <a class="app__btn right" href="#">Students</a>
+    </div>
+    <div class="container__statistics">
+        <h3 class="statistics__title">statistics</h3>
+        <p>Number of courses</p>
+    </div>
+</div>
