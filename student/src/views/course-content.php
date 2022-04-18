65ab331693c17ed270cc7194f053a3c4eabd9382
danieljml <danielgarrido425@gmail.com>
2022-04-18 15:16:42 -0400

    styles for course detail

--- student/src/views/course-content.php
+++ student/src/views/course-content.php
@@ -1,1 +1,37 @@
-<h1>Course # <?php echo $course["id"]; ?> view</h1>
+<h1 class="home_title">Course # <?php echo $course["id"]; ?> view</h1>
+<div class="body__container__course">
+<fieldset>
+      <legend class="title">Course Detail</legend>
+      <div class="container__student-course">
+        <div class="course__image">
+          <img
+            src="https://images.unsplash.com/profile-fb-1642446137-6bae7cc893b9.jpg?dpr=2&auto=format&fit=crop&w=60&h=60&q=60&crop=faces&bg=fff"
+            class="course__avatar"
+          />
+        </div>
+
+        <div class="course__content">
+          <div class="content__title-date">
+            <p class="course__title">Title: <br /><span>Matematica 1</span></p>
+            <p class="course__date">
+              Date: <br />
+              <span>17/04/2022</span>
+            </p>
+          </div>
+          <div class="content__teacher-level">
+            <p class="course__teacher">
+              Teacher: <br />
+              <span>Armando Duarte</span>
+            </p>
+            <p class="couser__level">
+              Level: <br />
+              <span>Level 1</span>
+            </p>
+          </div>
+        </div>
+        <div class="course__description">
+          Lorem ipsum dolor sit amet
+        </div>
+      </div>
+    </fieldset>
+</div>
