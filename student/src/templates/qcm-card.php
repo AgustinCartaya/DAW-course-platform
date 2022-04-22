88760d3053dd5d4896aff7cd84a3551d7edf888d
eguientat <eguientathomas@gmail.com>
2022-04-22 18:03:30 +0100

    qcm mit dans une course suivi et qcm adapté en fonction de la matière

--- student/src/templates/qcm-card.php
+++ student/src/templates/qcm-card.php
@@ -1,23 +1,34 @@
 
 <?php
-include(APP_QCM.'qcm.php');
+if(isset($_GET['type'])) {
+    // type index exists
 
+
+$t=$_GET['type'];
+//include(APP_QCM.'qcm_MATHEMATICS.php');
+include(APP_QCM.'qcm_'.$t.'.php');
 $qcm = new SimpleXMLElement($xmlstr);
 
 $i=0;
 while($i<=1){
 $j=0;
 ?>
-<form action="action.php" method="post">
+<form action="mailto:email@gmail.com" method="post">
+  <span>
   <p> <?php echo $qcm->items->item[$i]->Question;?> </p>
- <INPUT type="checkbox" name="choix1" value=<?php $qcm->items->item[$i]->answer[$j]?>> <?php echo $qcm->items->item[0]->answer[0]; $j++;?>
- <INPUT type="checkbox" name="choix1" value=<?php $qcm->items->item[$i]->answer[$j]?>> <?php echo $qcm->items->item[0]->answer[1]; $j++;?>
-   <INPUT type="checkbox" name="choix1" value=<?php $qcm->items->item[$i]->answer[$j]?>> <?php echo  $qcm->items->item[0]->answer[2]; $j++;?>
+
+ <INPUT type="radio" name="<?php echo $i;?>" value=<?php $qcm->items->item[$i]->answer[$j]?>> <?php echo $qcm->items->item[0]->answer[0]; $j++;?>
+ <INPUT type="radio" name="<?php echo $i;?>" value=<?php $qcm->items->item[$i]->answer[$j]?>> <?php echo $qcm->items->item[0]->answer[1]; $j++;?>
+   <INPUT type="radio" name="<?php echo $i;?>" value=<?php $qcm->items->item[$i]->answer[$j]?>> <?php echo  $qcm->items->item[0]->answer[2]; $j++;?>
+   </span>
 
 <?php
 $i++;
 }
+}
+
 ?>
 <p><input type="submit" value="OK"></p>
+
 
 </form>
