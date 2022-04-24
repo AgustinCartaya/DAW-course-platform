42c0417335305a3ae2b3dd236dd4dd6f42370fa9
danieljml <danielgarrido425@gmail.com>
2022-04-24 09:55:25 -0400

    forum styles

--- student/src/templates/forum-message.php
+++ student/src/templates/forum-message.php
@@ -1,4 +1,4 @@
-<div class="message__container">
+<div class="forum__container">
     <span><?php echo $message['user'];?></span>
     <p class="message__text"><?php echo $message['message'];?></p>
 </div>
